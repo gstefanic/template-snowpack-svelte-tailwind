5b5479ba8d2acfb918f04a0cdb15b4aa94c23c11
gstefanic <stefanic.gregor@gmail.com>
2020-11-21 20:23:22 +0100

    Initial commit.

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { onMount } from 'svelte';
+  let count = 0;
+  onMount(() => {
+    const interval = setInterval(() => count++, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  });
+</script>
+
+<style>
+  
+</style>
+
+<div class="bg-gray-100">
+  Hello World, {count}
+</div>
